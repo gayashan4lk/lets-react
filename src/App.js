e3414fe9cd331ee92c6e5478094d8660825da8fd
Gayashan Wagachchi <gayashan4lk@gmail.com>
2021-06-13 09:55:43 +0530

    complete the game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'
+import './App.css'
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +19,33 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App*/
+
+function App() {
+    return (
+        <div>
+            <h3>This is a simple component</h3>
+            <p>ssssfsfdfsd</p>
+            <span>##############</span>
+        </div>
+
+    )
+}
+
+export default App
+
+/*function Bacon() {
+    return (<h3>This is Bacon component</h3>)
+}
+
+export default Bacon*/
+
+/*function myComp() {
+    return (<h1>myComp component is here!</h1>)
+}
+export default myComp*/
+
+
