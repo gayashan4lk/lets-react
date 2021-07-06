615485d89f8598428c8fd688c859cd6eab149105
Gayashan Wagachchi <gayashan4lk@gmail.com>
2021-07-06 08:35:57 +0530

    rearrange code

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
-// import logo from './logo.svg'
+import React, {Component} from "react";
 import './App.css'
+import Game from "./components/Game";
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
-
-export default App*/
-
-function App() {
-    return (
-        <div>
-            <h3>This is a simple component</h3>
-            <p>ssssfsfdfsd</p>
-            <span>##############</span>
-        </div>
-
+class App extends Component {
+  render() {
+    return(
+      <Game />
     )
+  }
 }
 
-export default App
-
-/*function Bacon() {
-    return (<h3>This is Bacon component</h3>)
-}
-
-export default Bacon*/
-
-/*function myComp() {
-    return (<h1>myComp component is here!</h1>)
-}
-export default myComp*/
-
-
+export default App;
